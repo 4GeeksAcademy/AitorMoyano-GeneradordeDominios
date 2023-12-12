56ae0c57fb36f0731587f578c5c8a056d747b630
Aitor <62630503+AitorMoyano@users.noreply.github.com>
2023-12-12 18:26:57 +0000

    generadorDominios

--- app.js
+++ app.js
@@ -1,15 +1,27 @@
-
+window.onload = function() {
 
 
 let pronoun = ['the','our','his'];
 let adj = ['great','big','amazing'];
 let noun = ['jogger','racoon','dog'];
 
+var TextHTML = '<ul class="list-group">'
+
 for(let i = 0 ; i < pronoun.length; i++) {
     for(let j = 0; j < adj.length; j++){
         for(let k = 0; k < noun.length; k++){
-            console.log(pronoun[i] + adj[j] + noun[k] + '.com');
+            const element_i = pronoun[i];
+            const element_j = adj[j];
+            const element_k = noun[k];
+            let domainName = element_i + element_j + element_k + ".com";
+            TextHTML += '<li class="list-group-item">' + domainName + '</li>';
         }
     }
 }
+TextHTML += "</ul>";
+
+let element = document.querySelector('#domain');
+element.innerHTML = TextHTML;
+
+};
 
